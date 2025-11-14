a6cd2e9d6badb55d7447c759313c69e4912dde33
Mrseatree <378139419@qq.com>
2025-11-14 23:31:53 +0800

    新增返回结果枚举类以及返回结果，用于提示网络请求结果（成功、失败、具体失败原因等等）

---
+++ common/src/main/java/com/Books/common/mybatisplus/result/ResultCodeEnum.java
@@ -0,0 +1,18 @@
+package com.Books.common.mybatisplus.result;
+
+
+import lombok.Getter;
+
+@Getter
+public enum ResultCodeEnum {
+    SUCCESS(200, "成功"),
+    FAIL(201, "失败");
+
+    private final Integer code;
+    private final String message;
+
+    ResultCodeEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
