421051566c1728c7f96ee22b6697f5011ca0de3e
Mrseatree <378139419@qq.com>
2025-11-15 23:43:59 +0800

    web中依赖新增model模块，初始化我需要的mapper层、service层

---
+++ web/web-user/src/main/java/com/Books/web/app/mapper/NeedMapper.java
@@ -0,0 +1,8 @@
+package com.Books.web.app.mapper;
+
+import com.Books.model.entity.BookNeed;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface NeedMapper extends BaseMapper<BookNeed> {
+
+}
---
+++ web/web-user/src/main/java/com/Books/web/app/service/impl/NeedServiceImpl.java
@@ -0,0 +1,11 @@
+package com.Books.web.app.service.impl;
+
+import com.Books.model.entity.BookNeed;
+import com.Books.web.app.mapper.NeedMapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.Books.web.app.service.NeedService;
+
+public class NeedServiceImpl extends ServiceImpl<NeedMapper,BookNeed>
+    implements NeedService {
+
+}
